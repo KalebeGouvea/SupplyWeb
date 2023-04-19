03d603283d237932308f67abe6d755d4a1959e3c
Kalebe <Kalebe@DESKTOP-NAKLM4L>
2023-04-19 11:53:31 -0300

    Adicionar arquivos de projeto.

---
+++ src/SupplyWeb.Infra.IoC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyWeb.Infra.IoC
+{
+    public class Class1
+    {
+    }
+}
