870335f61c096e96c75e816099075e33604c6cbf
Kalebe Gouvea <k.gouvea@live.com>
2023-04-19 13:24:35 -0300

    refactor: entities refactoring

---
+++ src/SupplyWeb.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyWeb.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Manufacturer { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
