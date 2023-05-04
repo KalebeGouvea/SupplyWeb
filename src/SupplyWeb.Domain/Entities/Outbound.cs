b2b690ae43998408600adf30681873a9249c4e51
Kalebe Gouvea <k.gouvea@live.com>
2023-05-04 15:46:21 -0300

    fix: change entities display name

--- src/SupplyWeb.Domain/Entities/Outbound.cs
+++ src/SupplyWeb.Domain/Entities/Outbound.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SupplyWeb.Domain.Entities
 {
     public class Outbound
     {
         public int Id { get; set; }
+        [Display(Name = "Quantidade")]
         public int Quantity { get; set; }
+        [Display(Name = "Data e Hora")]
         public DateTime Date { get; set; }
+        [Display(Name = "Local")]
         public string Location { get; set; }
         public int ProductId { get; set; }
     }
--- src/SupplyWeb.Domain/Entities/Product.cs
+++ src/SupplyWeb.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace SupplyWeb.Domain.Entities
     public class Product
     {
         public int Id { get; set; }
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+        [Display(Name = "Número de Registro")]
         public string RegistrationNumber { get; set; }
+        [Display(Name = "Fabricante")]
         public string Manufacturer { get; set; }
+        [Display(Name = "Tipo")]
         public string Type { get; set; }
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
     }
 }
